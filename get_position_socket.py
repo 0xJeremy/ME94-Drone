6bb6ebe3208db69501ae2bad95587bd4df7cd029
0xJeremy <jeremy.kanovsky@tufts.edu>
2018-07-13 10:35:37 -0400

    Added python socket file

---
+++ get_position_socket.py
@@ -0,0 +1,48 @@
+import sys, json, numpy as np
+import json
+from marvelmind import MarvelmindHedge
+from time import sleep
+from contextlib import contextmanager
+import sys, os
+import socket
+
+def main():
+	socket_path = '/tmp/node-python-sock'
+	# connect to the unix local socket with a stream type
+	client = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+	client.connect(socket_path)
+    hedge = MarvelmindHedge(tty = "/dev/ttyACM0", adr=10, debug=False) # create MarvelmindHedge thread
+    hedge.start()
+    while True:
+        try:
+            x = hedge.position()
+            data = {}
+            data['x'] = x[0]
+            data['y'] = x[1]
+            data['z'] = x[2]
+            data['time'] = x[3]
+            jsonData = json.dumps(data)
+            # Might need to send as bytes...
+            client.send(jsonData)
+        except KeyboardInterrupt:
+        	client.close()
+            hedge.stop()  # stop and close serial port
+            sys.exit()
+
+
+
+# # send an initial message (as bytes)
+# client.send(b'python connected')
+# # start a loop
+# while True:
+#     # wait for a response and decode it from bytes
+#     msg = client.recv(2048).decode('utf-8')
+#     print(msg)
+#     if msg == 'hi':
+#         client.send(b'hello')
+#     elif msg == 'end':
+#         # exit the loop
+#         break
+
+# # close the connection
+# client.close()
