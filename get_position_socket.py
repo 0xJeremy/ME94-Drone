650e45251173bffb9546e7bd74520f0140bd422e
0xJeremy <jeremy.kanovsky@tufts.edu>
2018-07-17 14:50:43 -0400

    Moved control to Python

--- get_position_socket.py
+++ get_position_socket.py
@@ -6,6 +6,22 @@ from contextlib import contextmanager
 import sys, os
 import socket
 
+def get_desired_position(plane):
+	desired_pos = input('Enter desired ' + plane + ' coordinate: ')
+	return desired_pos
+
+def calculate_flight_power(position, desired_position):
+	diff = position - desired_position
+	margin = diff*0.05
+	if(diff <= margin):
+		return 0
+	else:
+		power = 0.1 * diff
+		if(power > 0.5):
+			return 0.5
+		else:
+			return power
+
 def main():
 	hedge = MarvelmindHedge(tty = "/dev/ttyACM0", adr=10, debug=False)
 	hedge.start()
@@ -13,15 +29,24 @@ def main():
 	# connect to the unix local socket with a stream type
 	client = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 	client.connect(socket_path)
+	desired_x = get_desired_position('x')
+	desired_y = get_desired_position('y')
 	while True:
 		try:
 			sleep(1)
 			x = hedge.position()
 			data = {}
-			data['x'] = x[0]
-			data['y'] = x[1]
-			data['z'] = x[2]
-			data['time'] = x[3]
+			# data['x'] = x[0]
+			# data['y'] = x[1]
+			# data['z'] = x[2]
+			# data['time'] = x[3]
+			flight_x = calculate_flight_power(x[0], desired_x)
+			flight_y = calculate_flight_power(x[1], desired_y)
+			# if(flight_x == 0 and flight_y == 0):
+			# 	desired_x = get_desired_position('x')
+			# 	desired_y = get_desired_position('y')
+			data['power_x'] = flight_x
+			data['power_y'] = flight_y
 			jsonData = json.dumps(data)
 			# Might need to send as bytes...
 			client.send(jsonData)
--- marvel_drone_socket.js
+++ marvel_drone_socket.js
@@ -1,19 +1,35 @@
 const net = require('net');
 const fs = require('fs');
 const socketPath = '/tmp/node-python-sock';
-var prompt = require('prompt');
+//var prompt = require('prompt');
 var arDrone = require('ar-drone');
 
 var client = arDrone.createClient();
-var drone_position;
-var desired_x;
-var desired_y;
+var flight_value;
+
+function fly_drone(flight_value) {
+	console.log("YAAAAAY");
+	console.log(flight_value);
+	if(flight_value.power_x < 0) {
+		client.left(flight_value.power_x);
+	}
+	else {
+		client.right(flight_value.power_x);
+	}
+	if(flight_value.power_y < 0) {
+		client.back(flight_value.power_y);
+	}
+	else {
+		client.front(flight_value.power_y);
+	}
+}
 
 const handler = (socket) => {
 	socket.on('data', (bytes) => {
 		const msg = bytes.toString();
-		drone_position = JSON.parse(msg);
-		//console.log(drone_position);
+		flight_value = JSON.parse(msg);
+		//console.log(flight_value);
+		fly_drone(flight_value);
 	});
 };
 fs.unlink(
@@ -21,37 +37,27 @@ fs.unlink(
 	() => net.createServer(handler).listen(socketPath)
 );
 
-function flight_value(current, desired) {
-	var diff = current-desired;
-	var margin = diff*0.05;
-	if(diff <= margin) {
-		return 0;
-	}
-	else {
-		var power = 0.1*diff;
-		if(power >= 0.6) {
-			return 0.6;
-		}
-	}
-}
+// function flight_value(current, desired) {
+// 	var diff = current-desired;
+// 	var margin = diff*0.05;
+// 	if(diff <= margin) {
+// 		return 0;
+// 	}
+// 	else {
+// 		var power = 0.1*diff;
+// 		if(power >= 0.6) {
+// 			return 0.6;
+// 		}
+// 	}
+// }
 
-function get_new_position() {
-	prompt.start();
-	prompt.get(['x'], function(err, result) {
-		desired_x = result.x;
-		//desired_y = result.y;
-		console.log('Command-line input received:');
-		console.log('x: ' + result.x);
-		//console.log('y: ' + result.y);
-	});
-}
-
-function main() {
-	console.log(drone_position);
-	get_new_position();
-	//console.log("Desired x: " + desired_x);
-	//console.log("Desired y: " + desired_x);
-	console.log(drone_position);
-}
-
-main();
+// function get_new_position() {
+// 	prompt.start();
+// 	prompt.get(['x'], function(err, result) {
+// 		desired_x = result.x;
+// 		//desired_y = result.y;
+// 		console.log('Command-line input received:');
+// 		console.log('x: ' + result.x);
+// 		//console.log('y: ' + result.y);
+// 	});
+// }
