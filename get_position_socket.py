a793279e925c14b7d591dbf34dae843f967f244d
0xJeremy <jeremy.kanovsky@tufts.edu>
2018-07-31 13:13:31 -0400

    Updated control for one plane

--- get_position_socket.py
+++ get_position_socket.py
@@ -39,22 +39,23 @@ def main():
 	client = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 	client.connect(socket_path)
 	desired_x, desired_y, flight_x, flight_y = 0, 0, 0, 0
-	data, y = {}, {0, 0, 0, 0, 0}
+	data, y, all_data = {}, {0, 0, 0, 0, 0}, {}
 	while True:
 		try:
 			sleep(1)
 			x = hedge.position()
 			x[1] = smooth_data(y[1], x[1])
 			x[2] = smooth_data(y[2], x[2])
+			add_data += x
 			y = x
 			print(x)
 			if(flight_x == 0 and flight_y == 0):
 				desired_x = get_desired_position('x')
 				desired_y = get_desired_position('y')
 			flight_x = calculate_flight_power(x[1], desired_x)
-			flight_y = calculate_flight_power(x[2], desired_y)
+			# flight_y = calculate_flight_power(x[2], desired_y)
 			data['power_x'] = flight_x
-			data['power_y'] = flight_y
+			# data['power_y'] = flight_y
 			data['time'] = x[4]
 			jsonData = json.dumps(data)
 			client.send(jsonData)
