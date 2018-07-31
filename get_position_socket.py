e4b44091301eeb0a7ab4f57a75cd8ca09918b359
0xJeremy <jeremy.kanovsky@tufts.edu>
2018-07-31 15:37:51 -0400

    Renamed variables, added comments

--- get_position_socket.py
+++ get_position_socket.py
@@ -7,10 +7,12 @@ import sys, os
 import socket
 import math
 
+# Gets user input for position
 def get_desired_position(plane):
 	desired_pos = input('Enter desired ' + plane + ' coordinate: ')
 	return desired_pos
 
+# Calculates the amount of thrust power
 def calculate_flight_power(position, desired_position):
 	diff = desired_position - position
 	margin = abs(diff*0.05)
@@ -25,6 +27,7 @@ def calculate_flight_power(position, desired_position):
 		else:
 			return power
 
+# Smoothes incoming data from MarvelMind
 def smooth_data(previous, current):
 	diff = current - previous
 	if(diff > (previous*0.1)):
@@ -33,6 +36,7 @@ def smooth_data(previous, current):
 		return (previous*0.9)
 	return current
 
+# Rotates MarvelMind data around the origin
 def rotate(origin, point, angle):
     ox, oy = origin
     px, py = point
@@ -40,48 +44,74 @@ def rotate(origin, point, angle):
     qy = oy + math.sin(angle) * (px - ox) + math.cos(angle) * (py - oy)
     return qx, qy
 
-# Rotate 136 degrees
 def main():
+
+	# Setup for MarvelMind and data socket
 	hedge = MarvelmindHedge(tty = "/dev/ttyACM0", adr=10, debug=False)
 	hedge.start()
 	socket_path = '/tmp/node-python-sock'
 	client = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 	client.connect(socket_path)
+
+	# Variable declarations and initalization
 	desired_x, desired_y, flight_x, flight_y = 0, 0, 0, 0
-	data, y = {}, []
-	all_data_x, all_data_y, all_data_time = [], [], []
+	data = {}
+	prev_position = []
+	data_log = []
 	first_run = True
 	rotation = 136
 	origin = (0, 0)
+
+	# Loop to get location and fly drone
 	while True:
 		try:
 			sleep(1)
+
+			# Code to run the first time
 			if(first_run):
 				initial_position = hedge.position()
-				y = initial_position
-				point = (y[1], y[2])
-				y[1], y[2] = rotate(origin, point, rotation)
+				prev_position = initial_position
+				point = (prev_position[1], prev_position[2])
+				prev_position[1], prev_position[2] = rotate(origin, point, rotation)
 				first_run = False
-			x = hedge.position()
-			x[1] = smooth_data(y[1], x[1]-initial_position[1])
-			x[2] = smooth_data(y[2], x[2]-initial_position[2])
-			point = (x[1], x[2])
-			x[1], x[2] = rotate(origin, point, rotation)
-			all_data_x.append(x[1])
-			all_data_y.append(x[2])
-			all_data_time.append(x[4])
-			y = x
-			print(x)
+
+			# Gets drone position and smooths data
+			position = hedge.position()
+			position[1] = smooth_data(prev_position[1], position[1]-initial_position[1])
+			position[2] = smooth_data(prev_position[2], position[2]-initial_position[2])
+
+			# Rotates the drone position
+			point = (position[1], position[2])
+			position[1], position[2] = rotate(origin, point, rotation)
+
+			# Adds position data to data log
+			data_log.append((position[1], position[2], position[4]))
+
+			# Sets current value as previous value
+			prev_position = position
+
+			# Displays location
+			print(position)
+
+			# Prompts user for desired flight location
 			if(flight_x == 0 and flight_y == 0):
 				desired_x = get_desired_position('x')
 				desired_y = get_desired_position('y')
-			flight_x = calculate_flight_power(x[1], desired_x)
-			# flight_y = calculate_flight_power(x[2], desired_y)
+
+			# Calculates the drone power
+			flight_x = calculate_flight_power(position[1], desired_x)
+			flight_y = calculate_flight_power(position[2], desired_y)
+
+			# Sets the flight values into a json format
 			data['power_x'] = flight_x
-			# data['power_y'] = flight_y
-			data['time'] = x[4]
+			data['power_y'] = flight_y
+			data['time'] = position[4]
 			jsonData = json.dumps(data)
+
+			# Sends data to JavaScript (marvel_drone_socket.js)
 			client.send(jsonData)
+
+		# Ends infinite loop and closes threads
 		except KeyboardInterrupt:
 			client.close()
 			hedge.stop()
